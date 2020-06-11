4713ff8156c6f992de394926bb19e91316cfaa6d
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-06-12 00:56:01 +0200

    git panels

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+  <v-expansion-panels>
+    <v-expansion-panel v-for="repo in repos" :key="repo.name">
+      <v-expansion-panel-header>{{ repo.name }}</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
+  const GitUrlParse = require("git-url-parse");
+
   export default {
     name: 'Home',
+    computed: {
+      repos: function() {
+        var rps = []
+        if (this.$store.state.workspace.config.repos) {
+          this.$store.state.workspace.config.repos.forEach(element => rps.push(GitUrlParse(element)));
+        }
+        return rps
+      },
+    }
   }
 </script>
