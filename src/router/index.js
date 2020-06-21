945832475eb5ee5d8ea2e9186f405cf1e923ef17
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-06-21 22:42:00 +0200

    repos

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,6 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Settings.vue')
   }
 ]
 
