183beab2b2d454b53e8403451cf61777a2771203
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-06-21 17:25:10 +0200

    add repo dialog

--- src/components/spaceActions.vue
+++ src/components/spaceActions.vue
@@ -1,20 +1,58 @@
 <template>
   <v-row justify="end">
-    <v-icon v-on:click="git_add" large>mdi-plus</v-icon>
-    <v-icon v-on:click="sync_all" large>mdi-cached</v-icon>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="amber"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon large>mdi-plus</v-icon>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title><span class="headline">New GIT</span></v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Url*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field label="Name*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field label="Branch*" required></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="git_add">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-btn color="amber"><v-icon v-on:click="sync_all" large>mdi-cached</v-icon></v-btn>
   </v-row>
 </template>
 
 <script>
   export default {
     name: 'spaceActions',
+    data: () => ({
+      dialog: false,
+    }),
     methods: {
       sync_all: function () {
-                  this.$socket.sendObj({ "topic": "git-sync-all", "payload": {}})
-                },
+            this.$socket.sendObj({ "topic": "git-sync-all", "payload": {}})
+          },
       git_add: function () {
-                  this.$socket.sendObj({ "topic": "git-add", "payload": {}})
-                }
+           this.dialog = false
+           this.$socket.sendObj({ "topic": "git-add", "payload": {}})
+         }
     }
   }
 </script>
